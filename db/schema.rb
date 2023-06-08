9266018b3c1776e69dbdf1f1ee0fcf0a11dabfad
Noah <noahmwangi@gmail.com>
2023-06-09 01:24:57 +0300

    Changes to Seeding File

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2023_06_08_212923) do
 
   create_table "movies", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
+    t.string "moviename"
+    t.string "actor"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 2023_06_08_212923) do
     t.integer "movie_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,29 @@
+
+
+require 'net/http'
+require 'json'
+
+puts "Deleting old data..."
+
+Review.destroy_all
+User.destroy_all
+
+# Reset auto-increment sequence for User table
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'users'")
+# Reset auto-increment sequence for Product table
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'movie'")
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+url = URI.parse("")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url.request_uri)
+
+response = http.request(request)
+
+puts response
 
 puts "✅ Done seeding!"
