9d72515757042b602456c06a3b99bc7851b54757
nicholas <nickkiim7@gmail.com>
2023-06-09 00:53:40 +0300

    migration-s

--- db/migrate/20230608212911_create_movies.rb.rb
+++ db/migrate/20230608212911_create_movies.rb.rb
@@ -1,8 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
     def change
       create_table :movies do |t|
-        t.string :name
-        t.string :author
+        t.string :moviename
+        t.string :actor
   
         t.timestamps
   end
