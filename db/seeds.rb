16267c250938a33aece166137cfd6fa1a149e178
Noah <noahmwangi@gmail.com>
2023-06-09 05:10:31 +0300

    seeding Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-
-
 require 'net/http'
 require 'json'
 
@@ -10,23 +8,53 @@ User.destroy_all
 
 # Reset auto-increment sequence for User table
 ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'users'")
-# Reset auto-increment sequence for Product table
-ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'movie'")
+# Reset auto-increment sequence for Movie table
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'movies'")
 
-puts "🌱 Seeding spices..."
-require 'net/http'
-require 'json'
-require 'active_record'
-
-# Assuming you have a Movie model with the necessary attributes and a connection to the database
+puts "🌱 Seeding movies..."
 
 # Fetch the JSON data from the API
-url = URI("https://api.themoviedb.org/3/discover/movie?api_key=4b8799a0185e3a736326d1c479824722&language=en-US")
-response = Net::HTTP.get(url)
-json_data = JSON.parse(response)
+url = URI("https://api.themoviedb.org/3/discover/movie")
+url_params = {
+  api_key: '4b8799a0185e3a736326d1c479824722',
+  language: 'en-US',
+  with_genres: '28,16,35,80,99,18,10751,9648,10766,10768,37,10770,10752',
+  'release_date.gte' => '2010-01-01',
+  'release_date.lte' => '2022-12-31',
+  'page' => 1, # Start from the first page
+}
 
-# Access the 'results' array in the JSON data
-movies_data = json_data['results']
+movies_data = [] # Array to store all movies data
+
+# Send multiple API requests to retrieve all pages of movie data
+loop do
+  # Send the API request and parse the JSON response
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  url.query = URI.encode_www_form(url_params)
+
+  request = Net::HTTP::Get.new(url)
+  request["Accept"] = 'application/json'
+
+  response = http.request(request)
+
+  if response.code == '200'
+    json_data = JSON.parse(response.body)
+
+    # Access the 'results' array in the JSON data and append it to the movies_data array
+    movies_data += json_data['results']
+
+    # Increment the page number for the next request
+    url_params['page'] += 1
+
+    # Break the loop if there are no more pages
+    break if json_data['page'] >= json_data['total_pages']
+  else
+    puts "❌ Error fetching movie data: #{response.code} - #{response.message}"
+    break
+  end
+end
 
 # Iterate over each movie data and create/update the corresponding record in the database
 movies_data.each do |movie_data|
@@ -36,12 +64,15 @@ movies_data.each do |movie_data|
   movie.title = movie_data['title']
   movie.description = movie_data['overview']
   movie.release_date = movie_data['release_date']
-  movie.movie_image = movie_data['movie_image']
-  # ... set other attributes as needed
+
+  # Check if the movie image URL is not null before assigning
+  if movie_data['poster_path'].nil?
+    movie.movie_image = 'https://via.placeholder.com/500x750' # Default image URL if no poster available
+  else
+    movie.movie_image = "https://image.tmdb.org/t/p/w500#{movie_data['poster_path']}"
+  end
 
   movie.save
 end
 
 puts "✅ Done seeding!"
-  
- 
