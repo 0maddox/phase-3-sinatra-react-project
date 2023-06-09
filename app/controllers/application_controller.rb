a723720405a6b0db512c8918cb9e4cd75d94b043
Noah <noahmwangi@gmail.com>
2023-06-09 06:53:45 +0300

    Changes to app_contr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   get '/' do
     movies = Movie.all
     movies.to_json
   end
-  
+
   get '/movies' do
     movies = Movie.all
     movies.to_json
@@ -16,26 +16,22 @@ class ApplicationController < Sinatra::Base
     users.to_json
   end
 
-  # Login endpoint
   post '/login' do
-    login_data = JSON.parse(request.body.read)
-
-    email = login_data['email']
-    password = login_data['password']
-
+    request_payload = JSON.parse(request.body.read)
+  
+    email = request_payload['email']
+    password = request_payload['password']
+  
     # Find the user by email
     user = User.find_by(email: email)
-
+  
     if user && user.authenticate(password)
-      status 200
       { message: 'Login successful' }.to_json
     else
-      status 401
       { message: 'Login failed' }.to_json
     end
   end
 
-  # Register endpoint
   post '/register' do
     register_data = JSON.parse(request.body.read)
 
@@ -43,10 +39,8 @@ class ApplicationController < Sinatra::Base
     email = register_data['email']
     password = register_data['password']
 
-    # Create a new user
-    user = User.new(name: name, email: email, password: password)
-
-    if user.save
+    # Add your registration logic here
+    if email && password
       status 200
       { message: 'Registration successful' }.to_json
     else
