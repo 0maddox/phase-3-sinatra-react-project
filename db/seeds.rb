254c760e02898335f2597c46941d55cd6709e793
Noah <noahmwangi@gmail.com>
2023-06-09 03:51:53 +0300

    Seeding nce again

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,66 +14,34 @@ ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 
 ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'movie'")
 
 puts "🌱 Seeding spices..."
+require 'net/http'
+require 'json'
+require 'active_record'
+
+# Assuming you have a Movie model with the necessary attributes and a connection to the database
+
+# Fetch the JSON data from the API
 url = URI("https://api.themoviedb.org/3/discover/movie?api_key=4b8799a0185e3a736326d1c479824722&language=en-US")
+response = Net::HTTP.get(url)
+json_data = JSON.parse(response)
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
+# Access the 'results' array in the JSON data
+movies_data = json_data['results']
 
-request = Net::HTTP::Get.new(url)
-request["Accept"] = 'application/json'
+# Iterate over each movie data and create/update the corresponding record in the database
+movies_data.each do |movie_data|
+  movie = Movie.find_or_initialize_by(id: movie_data['id'])
 
-response = http.request(request)
+  # Update the movie attributes
+  movie.title = movie_data['title']
+  movie.description = movie_data['overview']
+  movie.release_date = movie_data['release_date']
+  movie.movie_image = movie_data['movie_image']
+  # ... set other attributes as needed
 
-case response
-when Net::HTTPSuccess
-  body = JSON.parse(response.body)
-  movies = body['results']
-  
-  # Work with the movies data
-  movies.each do |movie|
-    puts movie['title']
-    puts movie['overview']
-    puts '---'
-  end
-else
-  puts "Error: #{response.code} - #{response.message}"
-end
-
-movies.each do |movie|
-  # Create the movie list
-  new_movie = Movie.create(
-  title: name["original_title"],
-  movie_image: movie["poster_path"],
-  description: movie["overview"],
-  release_date: movie["release_date"]
-)
-
+  movie.save
 end
 
 puts "✅ Done seeding!"
   
-  
-  # Define a route that handles the update request
-  put '/update_data' do
-    begin
-      # Parse the request body as JSON
-      request.body.rewind
-      data = JSON.parse(request.body.read)
-  
-     
-      # Return a success response
-  
-      status 200
-      body "Data updated successfully"
-    rescue JSON::ParserError => e
-      # Return a bad request response if the request body is not valid JSON
-      status 400
-      body "Invalid request body: #{e.message}"
-    rescue => e
-      # Log and return a server error response if an error occurs
-  
-      puts "Error updating data: #{e.message}"
-      status 500
-      body "Error updating data"
-    end
-  end
+ 
